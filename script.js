0314e39e3bdec0e1e10551f68537ad79893ca882
bygabrielamendes <gabriela_m_ferreira@outlook.com>
2025-05-03 18:47:53 -0300

    alteracao mozao

--- script.js
+++ script.js
@@ -2,24 +2,18 @@
 function mostrarSecao(secaoId) {
 
   // Esconde todas as seções da página
-  document.querySelectorAll('.section').forEach(secao => {
-    secao.classList.remove('active'); // Remove a classe 'active' (que deixa visível)
+  document.querySelectorAll('.secao').forEach(secao => {
+    secao.classList.remove('ativo'); // Remove a classe 'active' (que deixa visível)
   });
-
   // Mostra só a seção que foi clicada (adiciona a classe 'active')
-  document.getElementById(secaoId).classList.add('active');
-
+  document.getElementById(secaoId).classList.add('ativo');
+  
   // Remove o destaque (cor diferente) de todos os links do menu
   document.querySelectorAll('nav a').forEach(link => {
-    link.classList.remove('active');
+    link.classList.remove('ativo');
   });
 
   // Destaca o link do menu correspondente à seção clicada
   // Exemplo: se clicou em "projetos", ativa o link com id="link-projetos"
-  document.getElementById('link-' + secaoId).classList.add('active');
+  document.getElementById('link-' + secaoId).classList.add('ativo');
 }
-document.querySelectorAll('.icon').forEach(icon => {
-  icon.addEventListener('click', () => {
-    alert(`Você clicou no ícone de ${icon.classList[1]}`);
-  });
-});
